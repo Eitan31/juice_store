e913cc6212d1868911925c3c2f18733a3aa33d97
Eitan31 <145723798+Eitan31@users.noreply.github.com>
2025-01-03 01:18:36 +0200

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,7 @@ let currentUser = JSON.parse(localStorage.getItem("currentUser"));
 // פונקציה לשליפת מוצרים מהשרת
 async function fetchProducts() {
   try {
-    const response = await fetch('./src/json/products.json'');
+    const response = await fetch('./src/json/products.json');
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
